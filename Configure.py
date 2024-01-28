44c9fec6885b200b7d12370938a667f3c0f7220c
dhhdbd <90384921+dhhdbd@users.noreply.github.com>
2024-01-28 13:33:07 +0800

    Update Configure.py

--- Configure.py
+++ Configure.py
@@ -1,16 +1,16 @@
 # 必填
 # 用户名
-USER_NAME = ''
+USER_NAME = '周己泰'
 # 密码
-USER_PWD = ''
+USER_PWD = 'Z侯364615167'
 # 出发站
-FROM_STATION = '深圳北'
+FROM_STATION = '福州'
 # 到达站
-TO_STATION = '潮汕'
+TO_STATION = '信阳'
 # 乘车日期（格式: YYYY-mm-dd）
-TRAIN_DATE = '2018-02-27'
+TRAIN_DATE = '2024-02-3'
 # 购票人身份证号
-PASSENGERS_ID = ['4405xxxxx']
+PASSENGERS_ID = ['421302200106194814']
 # 票类型（单程:dc 往返:wc）
 TOUR_FLAG = 'dc'
 
@@ -20,7 +20,7 @@ TRAINS_NO = []
 # 座位类别（商务座(9),特等座(P),一等座(M),二等座(O),高级软卧(6),软卧(4),硬卧(3),软座(2),硬座(1),无座(1)）
 SEAT_TYPE_CODE = ['M', 'O', '4', '3', '2', '1']
 # 购票人类别（成人票:1,儿童票:2,学生票:3,残军票:4）
-PASSENGER_TYPE_CODE = '1'
+PASSENGER_TYPE_CODE = '3'
 # 座位选择 eg:['1A','2A'],有多少张票就填多少个,其中，A靠窗，B中间，C过道,D过道,F靠窗
 CHOOSE_SEATS = []
 
